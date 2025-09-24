cf6c6ebbfa4b941563c8e1bf89f3437da534b39d
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-09-24 21:13:19 +0530

    DAYS 27

---
+++ Q53.c
@@ -0,0 +1,41 @@
+/*Q53 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+*/
+#include <stdio.h>
+
+int main()
+ {
+           printf("Name-ANKUSH GULATI\nSAP ID-590020801\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    int i, j;
+    int n = 5; 
+
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= (2*i - 1); j++) { 
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    
+    for (i = n-1; i >= 1; i--) 
+    {
+        for (j = 1; j <= (2*i - 1); j++) 
+        { 
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
