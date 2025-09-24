b195c7e0a1b10c04474404eedfb8622b676c5b34
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-09-24 17:48:11 +0530

    DAYS 3

---
+++ Q6.c
@@ -0,0 +1,28 @@
+/*Q6 (User Inputs, Operations & Output)
+Write a program to swap two numbers using a third variable.*/
+#include <stdio.h>
+
+int main() 
+{
+    printf("Name-ANKUSH GULATI\nSAP ID-590020801\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    int a, b, temp;
+
+   
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("\nAfter swapping:\n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
+    getch();
+    return 0;
+}
