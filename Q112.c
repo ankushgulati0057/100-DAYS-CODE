debf5baae9b79377e6fe7c47f3ec619366497c61
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-11-03 23:27:54 +0530

    DAYS 62

---
+++ Q112.c
@@ -0,0 +1,36 @@
+/*Q112 (Logic Enhancers)
+Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element.*/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Name-ANKUSH GULATI\nSAP ID-590020801\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max_so_far = arr[0];
+    int current_sum = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        
+        if (current_sum + arr[i] > arr[i])
+            current_sum = current_sum + arr[i];
+        else
+            current_sum = arr[i];
+
+        
+        if (current_sum > max_so_far)
+            max_so_far = current_sum;
+    }
+
+    printf("Maximum subarray sum = %d\n", max_so_far);
+    return 0;
+}
