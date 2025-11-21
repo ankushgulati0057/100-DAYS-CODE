e11bcaa4e9de9385e230529e1a3e1741c2ff056a
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-11-21 20:44:30 +0530

    DAYS 83

---
+++ Q133.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+enum Status { SUCCESS, FAILURE, TIMEOUT };
+int main() 
+{
+    printf("Name-ANKUSH GULATI\nSAP ID-590020801\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    enum Months m;
+    printf("Enter month number (1-12): ");
+    scanf("%d", &m);
+
+    switch (m) {
+        case JAN: printf("January has 31 days\n"); break;
+        case FEB: printf("February has 28 or 29 days\n"); break;
+        case MAR: printf("March has 31 days\n"); break;
+        case APR: printf("April has 30 days\n"); break;
+        case MAY: printf("May has 31 days\n"); break;
+        case JUN: printf("June has 30 days\n"); break;
+        case JUL: printf("July has 31 days\n"); break;
+        case AUG: printf("August has 31 days\n"); break;
+        case SEP: printf("September has 30 days\n"); break;
+        case OCT: printf("October has 31 days\n"); break;
+        case NOV: printf("November has 30 days\n"); break;
+        case DEC: printf("December has 31 days\n"); break;
+        default: printf("Invalid month number\n");
+    }
+   getch();
+    return 0;
+}
