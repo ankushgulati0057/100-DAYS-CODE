48d9942e2533469ee3f4701b5d25e7abacd86670
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-11-21 20:46:44 +0530

    DAYS 89

---
+++ Q139.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum Numbers { A = 5, B = 10, C = 20, D = 30 };
+
+int main() 
+{
+      printf("Name-ANKUSH GULATI\nSAP ID-590020801\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    enum Numbers n;
+
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+    printf("D = %d\n", D);
+     getch();
+    return 0;
+}
